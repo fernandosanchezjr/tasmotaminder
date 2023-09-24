4ec01389a343cfd1f8ae5c1d616153a012ebe5e2
Fernando Sanchez, Jr <fernando.sanchez.jr@gmail.com>
2023-09-24 10:01:06 -0700

    clean up powerTimer logging

--- types/powertimer.go
+++ types/powertimer.go
@@ -10,7 +10,7 @@ type PowerTimer struct {
 	Power           int              `yaml:"power"`
 	PowerComparison *PowerComparison `yaml:"powerComparison,omitempty"`
 	RuntimeSeconds  int              `yaml:"runtimeSeconds"`
-	Action          *RuleAction      `yaml:"action"`
+	Action          *RuleAction      `yaml:"action,omitempty"`
 	mtx             sync.Mutex
 	started         bool
 	startTime       time.Time
@@ -22,29 +22,38 @@ func (pt *PowerTimer) Evaluate(plug *PlugState, target RuleTarget) {
 	defer mtx.Unlock()
 
 	if plug.sensorState.POWER1 == TasmotaPowerOFF {
+		log.Println("plug state", plug.sensorState.POWER1)
 		pt.started = false
 		return
 	}
 
+	log.Println("powerComparison:", plug.sensor.Energy.Power, pt.PowerComparison, pt.Power, "?")
+
 	if pt.PowerComparison.Compare(plug.sensor.Energy.Power, pt.Power) && !pt.started {
-		log.Println("powerTimer power check succeeded:", plug.sensor.Energy.Power, pt.PowerComparison, pt.Power)
 		pt.started = true
 		pt.startTime = time.Now()
 	} else if !pt.started {
 		return
 	}
 
+	log.Println("powerComparison succeeded")
+
 	runtime := int(time.Since(pt.startTime).Seconds())
+
+	log.Println(
+		"runtimeSeconds check:",
+		runtime,
+		">=",
+		pt.RuntimeSeconds,
+		"?",
+	)
+
 	if runtime < pt.RuntimeSeconds {
 		return
 	}
 
-	log.Println(
-		"powerTimer runtime check succeeded:",
-		runtime,
-		">=",
-		pt.RuntimeSeconds,
-	)
+	log.Println("runtimeSeconds succeeded")
+
 	pt.Action.Execute(target)
 	pt.started = false
 }
